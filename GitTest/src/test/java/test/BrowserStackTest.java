dcacf7d5a1ee044570db3452792f62aa947c623a
Basavaraj <basavarajpatil8767@gmail.com>
2025-01-03 10:26:40 +0530

    first push

--- GitTest/src/test/java/test/BrowserStackTest.java
+++ GitTest/src/test/java/test/BrowserStackTest.java
@@ -1,40 +1,42 @@
 package test;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import java.net.URL;
 
 public class BrowserStackTest {
-    public static void main(String[] args) {
-        // Set BrowserStack credentials
-        String userName = "basavarajapatil_YDIkUX";
-        String accessKey = "kRxFznJCJSS6Fzxw5wJa";
+	public static void main(String[] args) {
+		// Set BrowserStack credentials
+		String userName = "basavarajapatil_YDIkUX";
+		String accessKey = "kRxFznJCJSS6Fzxw5wJa";
 
-        // DesiredCapabilities for the browser
-        DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability("browser", "Chrome");
-        caps.setCapability("browser_version", "130");
-        caps.setCapability("os", "Windows");
-        caps.setCapability("os_version", "11");
-        caps.setCapability("name", "BrowserStack Test");
+		// DesiredCapabilities for the browser
+		DesiredCapabilities caps = new DesiredCapabilities();
 
-        try {
-            // Create a Remote WebDriver instance to trigger the BrowserStack session
-            WebDriver driver = new RemoteWebDriver(
-                    new URL("https://" + userName + ":" + accessKey + "@hub-cloud.browserstack.com/wd/hub"),
-                    caps
-            );
+		caps.setCapability("browser", "Chrome");
+		caps.setCapability("browser_version", "100");
+		caps.setCapability("os", "Windows");
+		caps.setCapability("os_version", "11");
+		caps.setCapability("name", "BrowserStack Test");
 
-            // Open a website
-            driver.get("http://www.fireflink.com");
+		try {
+			// Create a Remote WebDriver instance to trigger the BrowserStack session
+			WebDriver driver = new RemoteWebDriver(
+					new URL("https://" + userName + ":" + accessKey + "@hub-cloud.browserstack.com/wd/hub"), caps);
 
-            // Perform your test steps
-            System.out.println("Title of the page is: " + driver.getTitle());
+			// Open a website
+			driver.get("http://www.fireflink.com");
 
-            // Quit the driver session after the test is complete
-            driver.quit();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+			Thread.sleep(2000);
+
+			// Perform your test steps
+			System.out.println("Title of the page is: " + driver.getTitle());
+
+			// Quit the driver session after the test is complete
+			driver.quit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
